ab0d0e58e973951337d804bdca658240ea5161e9
Adrián Ramón <adrianramon2003@gmail.com>
2023-02-02 14:10:07 +0100

    Vistas creadas

---
+++ src/views/vistaInsertar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Insertar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'vistaInsertar'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/vistaMostrar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Mostrar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'vistaMostrar'
+}
+</script>
+
+<style>
+
+</style>
